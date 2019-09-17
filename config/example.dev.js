ee60e69f1ce7cdc200dc76056b08e89a693047eb
Hriday Panchasara <hhpancha@usc.edu>
2019-09-17 16:10:15 -0700

    Add files via upload

---
+++ config/example.dev.js
@@ -0,0 +1,11 @@
+module.exports={
+    googleClientID: 'xxxxx',
+    googleClientSecret: 'xxxxx-y0G',
+    mongoURI:'xxxxx',
+    cookieKey: 'random',
+    stripePublishableKey: 'xxxxx',
+    stripeSecretKey: 'xxxxx',
+    twilioSID: 'xxxxx',
+    twilioAuthToken: 'xxxxx',
+    twiioServiceSID: 'xxxxx'
+}; 
